30a355a25209b7f771d824bf61ccd0d05434371f
matruongne <matruong052003@gmail.com>
2024-12-27 18:38:56 +0700

    commit old project

---
+++ components/layout/footers.jsx
@@ -0,0 +1,24 @@
+
+
+import React from 'react';
+
+function Footers() {
+  return (
+    <footer className='footer'>
+      <h2>Beach Resort | Hotel Room Booking System</h2>
+      <p>
+        ©2023 Beach Resort — Developed By
+        {' '}
+        <a
+          href='https://sr.com'
+          rel='noreferrer'
+          target='_blank'
+        >
+          Ma TruongAdmin
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footers;
